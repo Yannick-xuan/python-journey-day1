637e1cf6148b8f942f159200e4456f33c5eef866
Yannick-xuan <weixuanhu66@gmail.com>
2025-07-07 17:52:43 +0100

    first commit

---
+++ scripts/hello.py
@@ -0,0 +1,10 @@
+name = input("what is your name?")
+print(f"my name is {name}")
+from datetime import date
+today = date.today()
+print(f"Today's date: {today}")
+
+number = 7
+square = number ** 2
+print(f"The square of {number} is {square}")
+
